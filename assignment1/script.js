4e0d919ed30c3a7d645a82f2077de56f18b48d90
palkit <dhiman.proff@gmail.com>
2025-07-09 23:44:06 +0530

    Assignment

---
+++ assignment1/script.js
@@ -0,0 +1,26 @@
+const gettingAccessOfData = (data) => {
+  const displayInHtml = document.getElementById("store");
+  data.map((product) => {
+    const productCard = document.createElement("div");
+    productCard.className = "product-card";
+    productCard.innerHTML = `
+      <img src="${product.thumbnail}" alt="${product.title}"/>
+      <h3>${product.title}</h3>
+      <p>${product.description}</p>
+      <p>Price : ${product.price}</p>
+    `;
+    displayInHtml.appendChild(productCard);
+  });
+};
+ 
+const getProducts = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    gettingAccessOfData(data.products);
+  } catch (error) {
+    console.log("error fetching products", error);
+  }
+};
+
+getProducts();
